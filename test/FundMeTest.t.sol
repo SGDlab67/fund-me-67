73e9809569e27486a2c5624389164cfb80e0996f
Stephen Le <huule@sungodlab.xyz>
2025-10-30 21:20:09 -0400

    Utilize Foundry Test Cheatcodes
    allow test protocols to give contract caller some eth to test the FundMe contract

--- test/FundMeTest.t.sol
+++ test/FundMeTest.t.sol
@@ -7,9 +7,15 @@ import {DeployFundMe} from "../script/DeployFundMe.s.sol";
 
 contract FundMeTest is Test {
     FundMe fundMe;
+
+    address USER = makeAddr("user");
+    uint256 constant SEND_VALUE = 0.1 ether; // 100000000000000000
+    uint256 constant STARTING_BALANCE = 10 ether;
+
     function setUp() external {
         DeployFundMe deployFundMe = new DeployFundMe();
         fundMe = deployFundMe.run();
+        vm.deal(USER, STARTING_BALANCE);
     }
 
     function testMinimumDollarIsFive() public {
@@ -17,7 +23,7 @@ contract FundMeTest is Test {
     }
 
     function testOwnerIsMsgSender() public {
-        // assertEq(fundMe.owner(), msg.sender);  
+        // assertEq(fundMe.owner(), msg.sender);
         // This will fail because the owner is set in the constructor to the address that deploys the contract, which is not msg.sender in this test context.
         assertEq(fundMe.i_owner(), msg.sender);
     }
@@ -28,8 +34,21 @@ contract FundMeTest is Test {
     // 4. Staging: Testing on a live network (testnet or mainnet)
 
     function testPriceFeedVersionIsAccurate() public {
-        uint256 version =fundMe.getVersion();
+        uint256 version = fundMe.getVersion();
         assertEq(version, 4);
     }
 
+    function testFundFailsWithoutEnoughETH() public {
+        vm.expectRevert(); // Hey, next line should revert
+        // assert(This tx fails/reverts)
+        fundMe.fund();
+    }
+
+    function testFundUpdatesFundedDataStructure() public {
+        vm.prank(USER); // The next tx will be sent by USER
+        fundMe.fund{value: SEND_VALUE}();
+
+        uint256 amountFunded = fundMe.getAddressToAmountFunded(USER);
+        assertEq(amountFunded, SEND_VALUE);
+    }
 }
